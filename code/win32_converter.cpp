17ea3eeb43c2954eaed5ff6a17ea44b291037b96
friendlyman23 <ams5661@gmail.com>
2025-03-28 16:22:55 -0700

    midstream on FlipEndianness function for extended data type

--- code/win32_converter.cpp
+++ code/win32_converter.cpp
@@ -12,7 +12,13 @@ typedef uint16_t uint16;
 typedef uint32_t uint32;
 typedef uint64_t uint64;
 
+typedef uint8_t uint8;
+typedef uint16_t uint16;
+typedef uint32_t uint32;
+typedef uint64_t uint64;
+
 #define Assert(Value) if(!(Value)) {*(int *)0 = 0;}
+#define ArrayCount(Array) sizeof(Array) / (sizeof(Array[0]))
 
 /*
  * The aif spec: https://www.mmsp.ece.mcgill.ca/Documents/AudioFormats/AIFF/AIFF.html 
@@ -28,6 +34,17 @@ typedef uint64_t uint64;
  * Per above, it is always 4 characters. We thus define it:
 */
 #define ID_WIDTH 4
+
+/*
+ * aif stores sample rate in an 80-bit (10-byte) floating point data type
+ * called "extended." MSVC doesn't support this type. We attempt to
+ * truncate it and store it in a double. We abort the conversion if
+ * the truncation would result in data loss.
+*/
+#define EXTENDED_WIDTH 10
+#define NUM_BITS_IN_BYTE 8
+
+
 
 void
 PrintDebugString(int32 i)
@@ -108,7 +125,7 @@ struct common_chunk
     int16 NumChannels;
     uint32 NumSampleFrames;
     int16 SampleSize;
-    float SampleRate;
+    double SampleRate;
 };
 
 void
@@ -165,6 +182,22 @@ ReadChunkDataStart(uint64 ChunkDataStart, form_chunk *ChunkStruct)
     ChunkStruct->DataStart = ChunkDataStart;
 }
 
+inline int16
+FlipEndianness(int16 IntToFlip)
+{
+    int16 IntToWrite;
+
+    uint8 IntToFlipByteZero = IntToFlip & 255;
+    uint8 IntToFlipByteOne = (IntToFlip >> 8) & 255;
+
+    uint16 IntToWriteByteOne = (uint16) IntToFlipByteZero << 8;
+    uint16 IntToWriteByteZero = (uint16) IntToFlipByteOne;
+
+    IntToWrite = (int16) (IntToWriteByteOne | IntToWriteByteZero);
+
+    return(IntToWrite);
+
+}
 
 inline int32 
 FlipEndianness(int32 IntToFlip)
@@ -187,16 +220,49 @@ FlipEndianness(int32 IntToFlip)
 			       IntToWriteByteZero);
 
     return(IntToWrite);
-
-    /*uint8 BE_LeastSignificant = *BEDataSize & 255;*/
-    /*uint8 BE_SecondByte = (*BEDataSize >> 8) & 255;*/
-    /*uint8 BE_ThirdByte = (*BEDataSize >> 16) & 255;*/
-    /*uint8 BE_MostSignificant = (*BEDataSize >> 24) & 255;*/
-    /**/
-    /*FormChunk.DataSize = (int32)(((uint32) BE_LeastSignificant << 24) | ((uint32) BE_SecondByte << 16) | ((uint32) BE_ThirdByte << 8) | ((uint32) BE_MostSignificant));*/
-
 }
 
+inline uint32 
+FlipEndianness(uint32 IntToFlip)
+{
+    uint32 IntToWrite;
+
+    uint8 IntToFlipByteZero = IntToFlip & 255;
+    uint8 IntToFlipByteOne = (IntToFlip >> 8) & 255;
+    uint8 IntToFlipByteTwo = (IntToFlip >> 16) & 255;
+    uint8 IntToFlipByteThree = (IntToFlip >> 24) & 255;
+
+    uint32 IntToWriteByteThree = (uint32) IntToFlipByteZero << 24;
+    uint32 IntToWriteByteTwo = (uint32) IntToFlipByteOne << 16;
+    uint32 IntToWriteByteOne = (uint32) IntToFlipByteTwo << 8;
+    uint32 IntToWriteByteZero = (uint32) IntToFlipByteThree;
+
+    IntToWrite = (uint32) (IntToWriteByteThree | 
+			       IntToWriteByteTwo |
+			       IntToWriteByteOne |
+			       IntToWriteByteZero);
+
+    return(IntToWrite);
+}
+
+inline double 
+FlipEndianness(uint8 *FirstByteOfExtended)
+{
+    double DoubleToWrite = 0;
+    uint8 ExtendedBytes[EXTENDED_WIDTH] = {};
+
+    uint8 *Index = FirstByteOfExtended;
+    for(int i = 0; i < EXTENDED_WIDTH; i++)
+    {
+	if((i >= 8) && (*Index != 0))
+	{
+	    return(DoubleToWrite);
+	}
+	ExtendedBytes[i] = *Index++;
+    }
+
+    return(DoubleToWrite);
+}
 
 int WinMain(HINSTANCE Instance, 
 	HINSTANCE PrevInstance, 
@@ -239,52 +305,39 @@ int WinMain(HINSTANCE Instance,
 
     common_chunk CommonChunk = {};
 
-    uint64 CommonChunkHeaderStart = FormChunk.DataStart;
+    uint8 *FileIndex = (uint8 *)FormChunk.DataStart;
+    uint64 CommonChunkHeaderStart = (uint64)FileIndex;
     ReadChunkAddress(CommonChunkHeaderStart, &CommonChunk);
 
     uint8 *CommonChunkIDStart = (uint8 *)CommonChunk.HeaderStart;
     ReadChunkID(CommonChunkIDStart, &CommonChunk);
 
-    int32 *CommonChunkDataSizeAddress = (int32 *)(CommonChunkHeaderStart + 
-						    ID_WIDTH);
+    FileIndex += ID_WIDTH;
+    int32 *CommonChunkDataSizeAddress = (int32 *)FileIndex;
     CommonChunk.DataSize = FlipEndianness(*CommonChunkDataSizeAddress);
-
     //per the spec, DataSize is always 18
     Assert(CommonChunk.DataSize == 18);
 
-    int16 CommonChunkNumChannels = (int16 *)(CommonChunk.HeaderStart + 
-						ID_WIDTH + 
-						sizeof(CommonChunk.DataSize));
+    FileIndex += sizeof(CommonChunk.DataSize);
+    int16 *CommonChunkNumChannels = (int16 *)FileIndex;
+    CommonChunk.NumChannels = FlipEndianness(*CommonChunkNumChannels);
 
+    FileIndex += sizeof(CommonChunk.NumChannels);
+    uint32 *CommonChunkNumSampleFrames = (uint32 *)FileIndex;
+    CommonChunk.NumSampleFrames = FlipEndianness(*CommonChunkNumSampleFrames);
 
+    FileIndex += sizeof(CommonChunk.NumSampleFrames);
+    int16 *CommonChunkSampleSize = (int16 *)FileIndex;
+    CommonChunk.SampleSize = FlipEndianness(*CommonChunkSampleSize);
 
+    FileIndex += sizeof(CommonChunk.SampleSize);
+    uint8 *CommonChunkSampleRate = FileIndex;
+    CommonChunk.SampleRate = FlipEndianness(CommonChunkSampleRate);
 
-
-
-
-
-
-
-
-
-
-
-
-    
-
-
-
-
-
-
-
-
-
-
-
-
-    
-
+/*7	   6	      5		 4*/
+/*98765432 10987654 32109876 54321098*/
+/*01000000 00001110 10101100 01000100 00000000 00000000 00000000 00000000 00000000 00000000 00000000*/
+/*SEEEEEEE EEEEIFFF FFFFFFFF FFFFFFFF*/
 
 
     return(0);
